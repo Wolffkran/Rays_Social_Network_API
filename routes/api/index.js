a486b10e1176bdffac99558ccadf9bf541faf9c2
Wolffkran <liebherr2008@live.com>
2024-01-31 15:46:17 -0500

    Setup index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+
+// Import user and thought routes
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
+// Set up routes
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
